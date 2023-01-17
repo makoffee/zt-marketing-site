940653d7d23d817b8634b03e152fe37bac8ea1df
makoffee <makoffee@antidata.net>
2023-01-17 16:38:53 +0100

    Theme updates

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,8 +1,8 @@
 export const colors = {
-  background: "#ffd500",
-  text: "#005bbb",
-  primary: "#005bbb",
-  muted: "#f5cc00",
-  active: "#004287",
+  background: "#1A1A1A",
+  text: "#fff",
+  primary: "#e5ff44",
+  muted: "#262626",
+  active: "#e5ff44",
   black: "#000",
 }
--- src/components/hero.js
+++ src/components/hero.js
@@ -18,15 +18,7 @@ export default function Hero(props) {
     <Section>
       <Container>
         <Flex gap={4} variant="responsive">
-          <Box width="half">
-            {props.image && (
-              <GatsbyImage
-                alt={props.image.alt}
-                image={getImage(props.image.gatsbyImageData)}
-              />
-            )}
-          </Box>
-          <Box width="half">
+        <Box width="half">
             <Heading as="h1">
               {props.kicker && <Kicker>{props.kicker}</Kicker>}
               {props.h1}
@@ -34,6 +26,14 @@ export default function Hero(props) {
             <Subhead as="h2">{props.subhead}</Subhead>
             <Text as="p">{props.text}</Text>
             <ButtonList links={props.links} />
+          </Box>
+          <Box width="half">
+            {props.image && (
+              <GatsbyImage
+                alt={props.image.alt}
+                image={getImage(props.image.gatsbyImageData)}
+              />
+            )}
           </Box>
         </Flex>
       </Container>
